cb30ad97ff5b0e441e3c2aecc657bdbe093602ef
Micaela V <micaela.v.mccall@gmail.com>
2023-07-27 14:14:56 -0600

    update model2

--- model2.py
+++ model2.py
@@ -25,34 +25,44 @@ pp_techniques = pp_techniques.rename(columns={'id': 'recipe_id'})
 # interactions_test_w_ingr = interactions_test_w_ingr.explode('ingredients')
 
 
+def literal_return(val):
+    try:
+        return literal_eval(val)
+    except (ValueError, SyntaxError) as e:
+        return val
+
 def add_deets_to_recipe(interactions, techniques, ingredients):
     interactions_w_deets = interactions.merge(techniques, how='left', on='recipe_id').merge(ingredients, how='left', on='recipe_id').drop(columns=['ingredient_ids'])
-    interactions_w_deets['ingredients'] = interactions_w_deets['ingredients'].apply(literal_eval)
-    interactions_w_deets['techniques'] = interactions_w_deets['techniques'].apply(literal_eval)
-    interactions_w_deets['flavors'] = interactions_w_deets['flavors'].apply(literal_eval)
+    interactions_w_deets['ingredients'] = interactions_w_deets['ingredients'].apply(literal_return)
+    interactions_w_deets['techniques'] = interactions_w_deets['techniques'].apply(literal_return)
+    interactions_w_deets['flavors'] = interactions_w_deets['flavors'].apply(literal_return)
     interactions_w_deets['deets'] = interactions_w_deets['ingredients'] + interactions_w_deets['techniques'] + interactions_w_deets['flavors']
     interactions_w_deets = interactions_w_deets.explode('deets')
     return interactions_w_deets
     
+# Add details to training data
 interactions_train_w_deets = add_deets_to_recipe(interactions_train, pp_techniques, ingr)
+interactions_train_w_deets.to_csv("data/interactions_train_w_deets.csv")
 interactions_train_deets = interactions_train_w_deets[['user_id', 'deets', 'rating', 'u']].drop_duplicates().reset_index(drop=True)
 interactions_train_deets = interactions_train_deets.groupby(['user_id', 'deets', 'u']).mean().reset_index()
 interactions_train_deets = interactions_train_deets[interactions_train_deets['deets'].str.len() > 0]
 interactions_train_deets.to_csv("data/interactions_train_deets.csv")
 
-
+# Add details to testing data
 interactions_test_w_deets = add_deets_to_recipe(interactions_test, pp_techniques, ingr)
-
 interactions_test_w_deets.to_csv("data/interactions_test_w_deets.csv")
 interactions_test_deets = interactions_test_w_deets[['user_id', 'deets', 'rating', 'u']].drop_duplicates().reset_index(drop=True)
 interactions_test_deets = interactions_test_deets.groupby(['user_id', 'deets', 'u']).mean().reset_index()
 interactions_test_deets = interactions_test_deets[interactions_test_deets['deets'].str.len() > 0]
 interactions_test_deets.to_csv("data/interactions_test_deets.csv")
 
+# Merge training ratings of details with testing true ratings of recipes
 pred_df = interactions_test_w_deets[['u', 'i', 'deets']].merge(interactions_train_w_deets[['u', 'deets', 'rating']], on=['u', 'deets'])
+# Group on recipes to get average of detail ratings as predicted recipe rating
 pred_df = pred_df.groupby(['u', 'i'])['rating'].mean().reset_index()
 pred_df.columns = ['u', 'i', 'rating_pred']
 
+# Merge back with testing data for evaluation
 eval_df = interactions_test[['u', 'i', 'rating']].merge(pred_df, how='inner', on=['i', 'u'])
 print(np.sqrt(mean_squared_error(eval_df['rating'], eval_df['rating_pred'])))
 print(mean_absolute_error(eval_df['rating'], eval_df['rating_pred']))
