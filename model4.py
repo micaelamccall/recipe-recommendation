b146c0b57fb52eb9cdcebbb6fb6eaa8153df3976
Micaela V <micaela.v.mccall@gmail.com>
2023-07-26 23:24:27 -0600

    update model4

--- model4.py
+++ model4.py
@@ -11,9 +11,18 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 interactions_train = pd.read_csv("data/interactions_train_mm.csv")
 interactions_test = pd.read_csv("data/interactions_test_mm.csv")
 
-interactions_test_w_deets = pd.read_csv("data/interactions_test_w_deets.csv")
-interactions_train_w_deets = pd.read_csv("data/interactions_train_w_deets.csv")[['user_id', 'recipe_id', 'deets', 'rating', 'u', 'i']].drop_duplicates().reset_index(drop=True)
-interactions_train_deets = interactions_train_w_deets.groupby(['user_id', 'deets', 'u']).mean().reset_index()
+interactions_test_w_deets = pd.read_csv("data/interactions_test_w_deets.csv")[['u', 'i', 'deets', 'rating']].drop_duplicates().reset_index(drop=True)
+interactions_train_w_deets = pd.read_csv("data/interactions_train_w_deets.csv")[['u', 'i', 'deets', 'rating']].drop_duplicates().reset_index(drop=True)
+
+deets = pd.concat([interactions_train_w_deets['deets'], interactions_test_w_deets['deets']]).drop_duplicates().reset_index(drop=True)
+
+deet_id_map = pd.DataFrame(deets).reset_index().rename(columns={'index':'d'})
+
+interactions_train_w_deets = interactions_train_w_deets.merge(deet_id_map, how='left', on='deets')
+interactions_test_w_deets = interactions_test_w_deets.merge(deet_id_map, how='left', on='deets')
+
+
+interactions_train_deets = interactions_train_w_deets.drop(columns=['deets']).groupby(['u', 'd']).mean().reset_index()
 
 num_users = np.max(interactions_train['u']) + 1
 num_recipes = np.max(interactions_train['i']) + 1
@@ -30,12 +39,15 @@ user_average = scsp.csr_matrix((user_average[nz[0]].T[0], nz))
 
 # Calculate normalized ratings matrix
 M_norm = M - user_average
-
+# Calculate similarity matrix
 sim = cosine_similarity(M_norm, dense_output=False)
 
+A = scsp.csr_matrix((interactions_train_deets['rating']+1, (interactions_train_deets['u'], interactions_train_deets['d'])))
 
-for u in interactions_test['u'].unique():
-    if u < 4:
+eval_df = pd.DataFrame()
+
+for u in interactions_test_w_deets['u'].unique():
+    if u < 5:
         print(u)
         # get similarities for that user and delete their similarity with themself
         simt = sim[u,:].toarray()
@@ -43,10 +55,26 @@ for u in interactions_test['u'].unique():
         # get top 50 most similar users 
         most_similar_users = np.argpartition(simt, -10)[-10:]
         # get the ratings for those most similar users 
-        similar_user_ratings = scsp.csr_matrix(M_norm[most_similar_users])
-        rated_recipes = np.unique(similar_user_ratings.nonzero()[1])
-        similar_user_ratings_exploded = interactions_train_w_deets[
-            (interactions_train_w_deets['u'].isin(most_similar_users)) & 
-            (interactions_train_w_deets['i'].isin(rated_recipes))].groupby(
-                ['user_id', 'deets', 'u']).mean().reset_index()
-        for recipe in interactions_test[interactions_test['u'] == 0]['i']
+        # similar_user_ratings = scsp.csr_matrix(M_norm[most_similar_users])
+        # rated_recipes = np.unique(similar_user_ratings.nonzero()[1])
+        # similar_user_deet_ratings = interactions_train_deets[
+        #     interactions_train_deets['u'].isin(most_similar_users)]
+        similar_user_deet_ratings = scsp.csr_matrix(A[most_similar_users])
+        
+        deet_score = scsp.csc_matrix(similar_user_deet_ratings.multiply(simt[most_similar_users].reshape(-1,1)) )
+        deet_pred = deet_score.sum(axis=0) / simt[most_similar_users].sum()
+        deet_pred[np.where(deet_pred > 0)] = deet_pred[deet_pred > 0]  + 1
+        deet_pred = pd.DataFrame(deet_pred.T).reset_index().rename(columns={'index': 'd', 0: 'deet_rating_pred'})
+        # deet_pred['u'] = u
+
+        pred_df = interactions_test_w_deets[interactions_test_w_deets['u'] == u]
+
+        pred_df = pred_df.merge(deet_pred, how='left', on='d')
+
+        pred_df = pred_df.groupby(['u', 'i'])['deet_rating_pred'].mean().reset_index().rename(columns={'deet_rating_pred':'rating_pred'})
+
+        eval_df = pd.concat([eval_df, pred_df])
+
+
+
+eval_df.merge(interactions_test_w_deets[['u', 'i', 'rating']].drop_duplicates(), how='left', on=['u', 'i'])
