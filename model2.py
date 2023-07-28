a4de2e86f0ee790c7c7524921bf4921186701eb0
Micaela V <micaela.v.mccall@gmail.com>
2023-07-27 21:18:59 -0600

    update model2

--- model2.py
+++ model2.py
@@ -7,17 +7,6 @@ from ast import literal_eval
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
-
-
-# interactions_train_w_ingr = interactions_train.merge(ingr, how='inner', on='recipe_id').drop(columns=['flavors', 'ingredient_ids'])
-# interactions_train_w_ingr['ingredients'] = interactions_train_w_ingr['ingredients'].apply(literal_eval)
-# interactions_train_w_ingr = interactions_train_w_ingr.explode('ingredients')
-# interactions_train_w_ingr = interactions_train_w_ingr.groupby(['u', 'ingredients'])['rating'].mean().reset_index()
-# interactions_test_w_ingr = interactions_test.merge(ingr, how='inner', on='recipe_id').drop(columns=['flavors', 'ingredient_ids'])
-# interactions_test_w_ingr['ingredients'] = interactions_test_w_ingr['ingredients'].apply(literal_eval)
-# interactions_test_w_ingr = interactions_test_w_ingr.explode('ingredients')
-
-
 interactions_train_w_deets = pd.read_csv("data/interactions_train_w_deets.csv")[['u', 'deets', 'rating']].drop_duplicates().reset_index(drop=True)
 interactions_test_w_deets = pd.read_csv("data/interactions_test_w_deets.csv")[['u', 'i', 'deets', 'rating']].drop_duplicates().reset_index(drop=True)
 
@@ -26,8 +15,7 @@ interactions_train_deets = interactions_train_deets[interactions_train_deets['de
 interactions_train_deets.to_csv("data/interactions_train_deets.csv")
 
 # Add details to testing data
-interactions_test_deets = interactions_test_w_deets[['deets', 'rating', 'u']].drop_duplicates().reset_index(drop=True)
-interactions_test_deets = interactions_test_deets.groupby(['deets', 'u']).mean().reset_index()
+interactions_test_deets = interactions_test_w_deets.groupby(['deets', 'u']).mean().reset_index()
 interactions_test_deets = interactions_test_deets[interactions_test_deets['deets'].str.len() > 0]
 interactions_test_deets.to_csv("data/interactions_test_deets.csv")
 
@@ -39,10 +27,12 @@ pred_df.columns = ['u', 'i', 'rating_pred']
 
 # Merge back with testing data for evaluation
 eval_df = interactions_test_deets[['u', 'i', 'rating']].merge(pred_df, how='inner', on=['i', 'u'])
+eval_df = eval_df.drop_duplicates()
 print(np.sqrt(mean_squared_error(eval_df['rating'], eval_df['rating_pred'])))
 print(mean_absolute_error(eval_df['rating'], eval_df['rating_pred']))
 
 eval_df.to_csv("results/model_2.csv")
-
+# 1.5794548509981838
+# 1.3070318485870909
 
 sns.scatterplot(data=eval_df, x='rating', y='rating_pred')
