e7c609c59656c131268f3eff76f409e5c18c3aa6
Micaela V <micaela.v.mccall@gmail.com>
2023-07-31 14:34:41 -0600

    update model1

--- model1.py
+++ model1.py
@@ -1,25 +1,23 @@
-import json
 import pandas as pd
 import scipy.sparse as scsp
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
-import scipy.stats as spst
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-interactions_train = pd.read_csv("data/interactions_train_mm.csv")
-interactions_test = pd.read_csv("data/interactions_test_mm.csv")
+interactions_train = pd.read_csv("data/interactions_train_mm.csv", index_col=0)
+interactions_test = pd.read_csv("data/interactions_test_mm.csv", index_col=0)
 
-# interactions_test[interactions_test['user_id'] == 1535]
-# g = interactions_test.groupby('u').nunique()['i']
+interactions_train['rating'] += 1
+interactions_test['rating'] += 1
 
 num_users = np.max(interactions_train['u']) + 1
 num_recipes = np.max(interactions_train['i']) + 1
 # create user-recipe matrix
 # add 1 to each rating so that all nonzero elements of the sparse matrix represent a rating
 
-M = scsp.csr_matrix((interactions_train['rating']+1, (interactions_train['u'], interactions_train['i'])))
+M = scsp.csr_matrix((interactions_train['rating'], (interactions_train['u'], interactions_train['i'])))
 
 # Calculate user level rating average
 user_average = np.array(M.sum(axis=1) / np.diff(M.indptr).reshape(-1,1))
@@ -31,7 +29,6 @@ user_average = scsp.csr_matrix((user_average[nz[0]].T[0], nz))
 M_norm = M - user_average
 
 sim = cosine_similarity(M_norm)
-
 
 eval_df = interactions_test[['u', 'i', 'rating']]
 eval_df.loc[:, 'rating_pred'] = np.nan
@@ -57,9 +54,10 @@ for u in interactions_test['u'].unique():
         # col_totals = score.sum(axis=0)[:, col_idx]
         # col_counts = [len(col_idx[np.where(col_idx == c)]) for c in col_idx]
         score_average = np.array(col_totals / col_counts)[0]
+        score_average[np.where(np.isnan(score_average) == True)] = 0
 
-        # Add in user average and subtract 1 because of adding it when making the matrix
-        score_prediction = score_average + np.max(user_average[u]) - 1
+        # Add in user average
+        score_prediction = score_average + np.max(user_average[u])
         eval_df.loc[eval_df['u']==u, 'rating_pred'] = score_prediction[eval_df[eval_df['u'] == u]['i']]
 
 
@@ -75,3 +73,23 @@ print(mean_absolute_error(eval_df['rating'], eval_df['rating_pred']))
 
 sns.scatterplot(data=eval_df, x='rating', y='rating_pred')
 
+from surprise import KNNWithMeans
+from surprise import BaselineOnly, Dataset, Reader, accuracy
+
+reader = Reader(rating_scale=(1, 6))
+
+traindata = Dataset.load_from_df(interactions_train[["u", "i", "rating"]], reader)
+traindata = traindata.build_full_trainset()
+
+
+testdata = Dataset.load_from_df(interactions_test[["u", "i", "rating"]], reader)
+testdata = testdata.build_full_trainset().build_testset()
+
+algo = KNNWithMeans(sim_options={'name':'cosine'})
+algo.fit(traindata)
+
+predictions = algo.test(testdata)
+accuracy.rmse(predictions, verbose=True)
+
+predictions = pd.DataFrame.from_records([{'u': pred.uid, 'u': pred.iid, 'rating': pred.r_ui, 'rating_pred': pred.est} for pred in predictions])
+sns.scatterplot(data=predictions, x='rating', y='rating_pred')
